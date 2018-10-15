80fd85d0e234a1d84adddd9f64bf1820b578f7d3
Rafed <rafed123@yahoo.com>
2018-10-16 03:20:51 +0600

    First commmit

---
+++ try.go
@@ -0,0 +1,21 @@
+package maia
+
+import "fmt"
+import "strings"
+
+func main() {
+	hello := "Hello"
+	world := "World"
+	words := []string{hello, world}
+	SayHello(words)
+}
+
+// SayHello says Hello
+func SayHello(words []string) {
+	fmt.Println(joinStrings(words))
+}
+
+// joinStrings joins strings
+func joinStrings(words []string) string {
+	return strings.Join(words, ", ")
+}
